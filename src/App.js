805f05d0c4c7fb2f381de6e4166577794afe2f63
M. Yasar Ozden <myozden@gmail.com>
2022-10-20 15:44:50 +0300

    yeni dosyalar var

--- src/App.js
+++ src/App.js
@@ -4,20 +4,62 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React from MYO
-        </a>
-      </header>
+
+      <div className='header'>
+        <h1>My Website</h1>
+        <p>Resize the browser window to see the effect.</p>
+      </div>
+      <div className="topnav">
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+        <a href="#">Link</a>
+      </div>
+
+      <div className="row">
+        <div className="leftcolumn">
+          <div className="card">
+            <h2>TITLE HEADING</h2>
+            <h5>Title description, Dec 7, 2017</h5>
+            <div className="fakeimg">Image</div>
+            <p>Some text..</p>
+            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+          </div>
+          <div className="card">
+            <h2>TITLE HEADING</h2>
+            <h5>Title description, Sep 2, 2017</h5>
+            <div className="fakeimg">Image</div>
+            <p>Some text..</p>
+            <p>Sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco.</p>
+          </div>
+        </div>
+        <div className="rightcolumn">
+          <div className="card">
+            <h2>About Me</h2>
+            <div className="fakeimg">Image</div>
+            <p>Some text about me in culpa qui officia deserunt mollit anim..</p>
+          </div>
+          <div className="card">
+            <h3>Popular Post</h3>
+            <div className="fakeimg"><p>Image</p></div>
+            <div className="fakeimg"><p>Image</p></div>
+            <div className="fakeimg"><p>Image</p></div>
+          </div>
+          <div className="card">
+            <h3>Follow Me</h3>
+            <p>Some text..</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="footer">
+        <h2>Footer</h2>
+      </div>
+
+
+
+
+
+
     </div>
   );
 }
